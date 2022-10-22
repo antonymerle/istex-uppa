4fbef15476db5bc3554024111dda8b3f2119b6b7
Antony Merle <antony.merle@gmail.com>
2022-10-22 22:46:53 +0200

    debug index

--- src/app/services/istex.service.ts
+++ src/app/services/istex.service.ts
@@ -14,12 +14,12 @@ export class IstexService {
   results: IstexRecord[] = [];
   paginator: Paginator = {
     RANGE: 5,
-    pageIndex: -1,
+    pageIndex: 0,
   };
 
   BSResult = new BehaviorSubject(this.results);
   BSApiResponse = new BehaviorSubject(this.apiResponse);
-  BSPageIndex = new BehaviorSubject(0);
+  // BSPageIndex = new BehaviorSubject(0);
 
   constructor(private http: HttpClient) {}
 
@@ -32,7 +32,7 @@ export class IstexService {
     this.http.get<APIResult>(this.apiURL + query).subscribe((data) => {
       this.BSApiResponse.next(data);
       this.BSResult.next(data.hits);
-      if (this.paginator.pageIndex >= 0) {
+      if (this.paginator.pageIndex > 0) {
         // reset état car le paginateur était déjà déclenché par une précédente recherche
         this.paginator.pageIndex = 0;
       }
@@ -73,14 +73,14 @@ export class IstexService {
     console.log('getPreviousResults');
   }
 
-  getIndexPageResults(index: number) {
+  getPageResultsByPageIndex(index: number) {
     if (index > Math.ceil(this.BSApiResponse.getValue().total / 10)) {
-      // 10 = nombre de résultat par page. TODO : à rendre dynamique
+      // 10 = nombre de résultats par page. TODO : à rendre dynamique
       return;
     }
     const apiURLFromIndex =
       this.BSApiResponse.getValue().firstPageURI?.slice(0, -1) +
-      (index * 10).toString(); // _API_URL_&from=0 -> _API_URL_&from=index*10
+      ((index - 1) * 10).toString(); // _API_URL_&from=0 -> _API_URL_&from=index*10
     console.log(apiURLFromIndex);
 
     this.http.get<APIResult>(apiURLFromIndex).subscribe((data) => {
