a3a8629b66e50b24865c950513f4acab951fb27a
Antony Merle <antony.merle@gmail.com>
2022-10-22 22:56:27 +0200

    prevPageURI instead of lastPageURI

--- src/app/APIResult.ts
+++ src/app/APIResult.ts
@@ -3,6 +3,7 @@ import { IstexRecord } from './IstexRecord';
 export interface APIResult {
   total: number;
   nextPageURI?: string;
+  prevPageURI?: string;
   firstPageURI?: string;
   lastPageURI?: string;
   hits: IstexRecord[];
