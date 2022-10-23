34fd5a31bfc311890db12a83062b770220e93d32
Antony Merle <antony.merle@gmail.com>
2022-10-23 16:33:52 +0200

    resultats plus complets de l'API

--- src/app/IstexRecord.ts
+++ src/app/IstexRecord.ts
@@ -1,6 +1,19 @@
 export interface IstexRecord {
-  arkIstex: string;
   title: string;
+  corpusName: string;
+  author: Array<{ name: string; affiliations: Array<string> }>;
+  subject: Array<string>;
+  language: Array<string>;
+  abstract: string;
+  host: {
+    title: string;
+    volume: string;
+    issue: string;
+    pages: { first: string; last: string };
+  };
+  publicationDate: string;
+  doi: string;
+  fulltext: Array<{ extension: string; mimetype: string; uri: string }>;
   id: string;
-  score: number;
+  // score: number;
 }
